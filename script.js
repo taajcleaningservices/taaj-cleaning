4512309752c2bfe41600d1edd5ac9b0fa648f844
taajcleaningservices <taajcleaningservices@gmail.com>
2026-04-27 02:40:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 function scrollToQuote(){
     document.getElementById("contact").scrollIntoView({
-        behavior: "smooth"
+        behavior:"smooth"
     });
 }
 
-/* FORM */
-document.getElementById("quoteForm").addEventListener("submit", function(e){
+document.getElementById("form").addEventListener("submit", function(e){
     e.preventDefault();
 
-    document.getElementById("quoteForm").style.display = "none";
-    document.getElementById("thankYou").classList.remove("hidden");
+    this.style.display="none";
+    document.getElementById("thanks").style.display="block";
 
     setTimeout(()=>{
         location.reload();
     },3000);
-});
-
-/* TEXT ANIMATION */
-const reveals = document.querySelectorAll(".reveal");
-
-window.addEventListener("scroll", () => {
-    reveals.forEach(el => {
-        const top = el.getBoundingClientRect().top;
-        if(top < window.innerHeight - 100){
-            el.classList.add("active");
-        }
-    });
 });
